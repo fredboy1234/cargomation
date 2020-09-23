0859b68522ac3abf1985097cc19a31f0f9a56422
johnalexladra <johnalexladra@gmail.com>
2020-09-23 23:17:39 +0800

    final backbone

--- public/index.php
+++ public/index.php
@@ -1,5 +1,38 @@
 <?php
-echo "Test";
-echo "Test1";
+/**
+ * Internal Error handling
+ */
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-?>
+/**
+ * Front controller
+ *
+ * PHP version 7.0
+ */
+
+/**
+ * Composer
+ */
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+
+/**
+ * Error and Exception handling
+ */
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+
+/**
+ * Routing
+ */
+$router = new Core\Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('{controller}/{action}');
+    
+$router->dispatch($_SERVER['QUERY_STRING']);
