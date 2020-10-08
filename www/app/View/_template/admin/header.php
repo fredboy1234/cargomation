d8703f83f1d8176f6c560b43791604b7133c444a
johnalexladra <johnalexladra@gmail.com>
2020-10-09 06:22:10 +0800

    Updated admin template

--- www/app/View/_template/admin/header.php
+++ www/app/View/_template/admin/header.php
@@ -30,3 +30,6 @@
   <!-- css -->
   <?= $this->getCSS(); ?>
 </head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
--- www/app/View/_template/admin/sidebar.php
+++ www/app/View/_template/admin/sidebar.php
@@ -1,7 +1,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="/" class="brand-link">
       <img src="/bower_components/admin-lte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">A2B Freight Hub</span>
@@ -27,7 +27,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="#" class="nav-link active">
+            <a href="/" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
