ac71087d78048ee9aa9432f5aaa6490c58dec176
johnalexladra <johnalexladra@gmail.com>
2020-10-12 19:43:05 +0800

    Initial add for Client, rename menu_sidebar to sidebar

---
+++ www/app/View/_template/client/sidebar.php
@@ -0,0 +1,89 @@
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="/" class="brand-link">
+      <img src="/bower_components/admin-lte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">A2B Freight Hub</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <?php if (isset($this->data)): ?>
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="/bower_components/admin-lte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block"><?= $this->escapeHTML($this->data->name); ?></a>
+        </div>
+      </div>
+      <?php endif; ?>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="/" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/admin/profile" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Profile
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+          <?php if(false): ?>
+          <li class="nav-item">
+            <a href="/admin/shipment" class="nav-link">
+              <i class="nav-icon fas fa-ship"></i>
+              <p>
+                Shipment
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/admin/transport" class="nav-link">
+              <i class="nav-icon fas fa-truck"></i>
+              <p>
+                Transport
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="/admin/logs" class="nav-link">
+              <i class="nav-icon fas fa-file"></i>
+              <p>
+                User Logs
+                <span class="right badge badge-danger">New</span>
+              </p>
+            </a>
+          </li>
+          <?php endif; ?>
+          <li class="nav-item">
+            <a href="/login/logout" class="nav-link">
+              <i class="nav-icon fas fa-door-open"></i>
+              <p>
+                Log Out
+              </p>
+            </a>
+          </li>
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
