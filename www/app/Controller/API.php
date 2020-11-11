db23dd94672f196054386f53b5367b3c4d3460b6
johnalexladra <johnalexladra@gmail.com>
2020-11-11 11:22:29 +0800

    Added Shipment API controller and view

---
+++ www/app/Controller/API.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core;
+use App\Model;
+use App\Utility;
+use App\Presenter;
+
+use App\Core\View;
+
+/**
+ * API Controller:
+ *
+ * @author John Alex
+ * @since 1.0
+ */
+
+class API extends Core\Controller {
+
+    public $shipment_id;
+
+    public function index() {
+        $this->View->renderTemplate("admin", "admin/index", [
+            "title" => "API"
+        ]);
+    }
+
+    public function shipment ($shipment_id = "") {
+
+        $api_url = "http://a2bfreighthub.com/eAdaptor/jsoneAdaptor.php?shipment_id=" . $shipment_id;
+
+        $this->View->render("api/shipment", [
+            "title" => "Shipment API",
+            "shipment" => file_get_contents($api_url)
+        ]);
+    }
+
+}
