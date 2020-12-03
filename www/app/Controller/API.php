4ad21ba6ef1d53978f952692437a28141845b4eb
johnalexladra <johnalexladra@gmail.com>
2020-12-03 18:35:40 +0800

    Update controller Api(future implementation)

--- www/app/Controller/API.php
+++ www/app/Controller/API.php
@@ -36,4 +36,14 @@ class API extends Core\Controller {
         ]);
     }
 
+    public function document ($shipment_id = "") {
+
+        $api_url = "http://a2bfreighthub.com/eAdaptor/jsoneAdaptor.php?shipment_id=" . $shipment_id;
+
+        $this->View->render("api/shipment", [
+            "title" => "Shipment API",
+            "shipment" => file_get_contents($api_url)
+        ]);
+    }
+
 }
