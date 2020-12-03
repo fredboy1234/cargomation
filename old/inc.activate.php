9b4562ed26e25d06ac56b88b7ff94e1babffc902
johnalexladra <johnalexladra@gmail.com>
2020-12-03 16:24:12 +0800

    Fix minor error in old hub

--- old/inc.activate.php
+++ old/inc.activate.php
@@ -1,5 +1,5 @@
 <?php
-$email = $_GET['xpathemail']
+$email = $_GET['xpathemail'];
 $sqlUpdate ="UPDATE tblLogin INNER JOIN tblClient ON tblLogin.loginID = tblClient.clientID SET tblClient.clientIsActive = 'Y'
 WHERE (((tblLogin.loginEmail)='$email') AND ((tblClient.clientIsActive)='N'));"
 
