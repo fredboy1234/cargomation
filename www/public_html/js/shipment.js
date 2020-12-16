07ff98b46237112bef9ff1cc239b64a8624e0a3e
johnalexladra <johnalexladra@gmail.com>
2020-12-17 01:05:00 +0800

    Fix url in ajax

--- www/public_html/js/shipment.js
+++ www/public_html/js/shipment.js
@@ -95,7 +95,7 @@ $(document).ready(function(){
       processing: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span> '},
     serverMethod: 'get',
     ajax: {
-      url:'http://basin.con/admin/shipmentSSR/',
+      url: document.location.origin + '/admin/shipmentSSR/',
     },
     columns: [
       { data: 'shipment_id' },
