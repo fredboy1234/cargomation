50b558ea51b436db6822fbe43c1ace9aface4e52
neil <neildesucatan@gmail.com>
2020-12-16 19:35:12 +0800

    remove old code for advance search

--- www/public_html/js/shipment.js
+++ www/public_html/js/shipment.js
@@ -113,52 +113,6 @@ $("#advance-search-btn").on("click",function(){
   table.clear();
   table.draw();
 });
-// $("#advance-search-btn").on("click",function(){
-//   var data = $("#addvance-search-form").serializeArray();
-//   var query_data = {};
-//   var html="";
-//   $.each(data,function(key,value){
-//     var index = value.name;
-//     var val = value.value;
-//     query_data[index] = val;
-//   });
-  
-//   $.ajax({
-//     type: "POST",
-//     url: "/admin/advanceSearch/",
-//     ContentType: 'application/json',
-//     data: query_data,
-//     beforeSend: function(){
-//       $("table tbody").html('<tr class="odd"><td valign="top" colspan="9" class="dataTables_empty">Loading...</td></tr>');
-//     },
-//     success: function(response){
-//       console.log($.parseJSON(response));
-//      if(response.length > 2){
-//         $.each(JSON.parse(response),function(key,val){
-//         var date_etd = new Date(val.etd);
-//         var etd_date= (date_etd.getMonth() + 1) + '/' + date_etd.getDate() + '/' +  date_etd.getFullYear(); 
-//         var date_eta = new Date(val.eta);
-//         var eta_date= (date_eta.getMonth() + 1) + '/' + date_eta.getDate() + '/' +  date_eta.getFullYear(); 
-//         html += `<tr>
-//                   <td>${val.ex_shipment_num}</td>
-//                   <td>${(val.console_id != "" ? val.console_id : "No Console ID") }</td>
-//                   <td>${eta_date}</td>
-//                   <td>${etd_date}</td>
-//                   <td><span class="doc badge badge-success" data-type="HBL" data-id="${val.shipment_num}">Approved</span></td>
-//                   <td><span class="doc badge badge-success" data-type="CIV" data-id="${val.shipment_num}">Approved</span></td>
-//                   <td><span class="doc badge badge-warning" data-type="PKL" data-id="${val.shipment_num}">Pending</span></td>
-//                   <td><span class="doc badge badge-success" data-type="PKD" data-id="${val.shipment_num}">Approved</span></td>
-//                   <td><span class="doc badge badge-success" data-id="${val.shipment_num}">Approved</span></td>
-//                   <td>${typeof(val.comment)!="undefined" ? val.comment : "<em>No comment</em>"}</td>
-//                 </tr>`;
-//       });
-//      }else{
-//        html ='<tr class="odd"><td valign="top" colspan="9" class="dataTables_empty">No matching records found</td></tr>';
-//      }
-//      $("table tbody").html(html);
-//     }
-//   });
-// });
 
 // Toggle document stats view 
 $(document).on('mouseenter mouseleave','.stats',function() {
