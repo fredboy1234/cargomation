843284814c80c75f34cd2181f10126af7f282671
johnalexladra <johnalexladra@gmail.com>
2020-12-17 18:05:15 +0800

    Fix login

--- www/app/View/login/index.php
+++ www/app/View/login/index.php
@@ -268,7 +268,7 @@ $(document).ready(function(){
 
       if( email != "" && password != "" ){
           $.ajax({
-              url:'<?= $this->makeUrl("login/_login"); ?>',
+              url:'<?= $this->makeUrl("/login/_login"); ?>',
               type:'post',
               data:{email:email,password:password,csrf_token:csrf_token},
               beforeSend: function() {
