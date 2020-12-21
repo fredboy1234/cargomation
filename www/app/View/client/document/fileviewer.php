02a972a356c73e825cc2dc35ff03af54e8e1a77b
johnalexladra <johnalexladra@gmail.com>
2020-12-21 12:55:12 +0800

    fix: document fileviewer

--- www/app/View/client/document/fileviewer.php
+++ www/app/View/client/document/fileviewer.php
@@ -6,29 +6,28 @@ $file_name = $file[0]->name;
 $shipment_num = $file[0]->shipment_num;
 $file_type = $file[0]->type;
 
-$url = "http://a2bfreighthub.com/filemanager/".$this->email."/CW_FILE/".$shipment_num."/".$file_type."/" . rawurlencode($file_name);
+// URL: http://a2bfreighthub.com/filemanager/cto@mail.com/CW_FILE/S00001055/MSC/Coversheet%20-%20S00001055.pdf
+$file = "E:/A2BFREIGHT_MANAGER/".$this->email."/CW_FILE/".$shipment_num."/".$file_type."/" . $file_name;
 
-$cookie = 'cookies.txt';
-$timeout = 30;
+try {
+    $fp = fopen($file, "r") ;
 
-$ch = curl_init(); 
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_TIMEOUT, 10); 
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout );
-curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
-curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
-// curl_setopt($CurlConnect, CURLOPT_POSTFIELDS, $request);
-// curl_setopt($CurlConnect, CURLOPT_USERPWD, $login.':'.$password);
-//curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Encoding: none','Content-Type: application/pdf')); 
+    header("Cache-Control: maxage=1");
+    header("Pragma: public");
+    header("Content-type: application/pdf");
+    header("Content-Disposition: inline; filename=".$file_name."");
+    header("Content-Description: PHP Generated Data");
+    header("Content-Transfer-Encoding: binary");
+    header('Content-Length:' . filesize($file));
+    ob_clean();
+    flush();
+    while (!feof($fp)) {
+       $buff = fread($fp, 1024);
+       print $buff;
+    }
+} catch (\Throwable $th) {
+    //throw $th;
+    echo "Preview only available in live.";
+}
 
-$result = curl_exec($ch);
-header('Cache-Control: public'); 
-header('Content-type: application/pdf');
-header('Content-Disposition: inline; filename="'.$file[0]->name.'"');
-header('Content-Length: '.strlen($result));
-curl_close($ch);
-echo $result;
-
-?>
+exit;
