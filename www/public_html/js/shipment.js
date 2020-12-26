e6eb137cc888ffed13b9ade8608a3f653b49f81a
neil <neildesucatan@gmail.com>
2020-12-26 10:06:16 +0800

    add: Predefined Date Ranges

--- www/public_html/js/shipment.js
+++ www/public_html/js/shipment.js
@@ -87,11 +87,22 @@ $('#doc_search').on( 'keyup', function () {
 
 
 //ETA time picker
+var start = moment().subtract(29, 'days');
+var end = moment();
+
 $('input[name="ETA"]').daterangepicker({
   //timePicker: true,
   startDate: moment().startOf('hour'),
   endDate: moment().startOf('hour').add(32, 'hour'),
   autoUpdateInput: false,
+  ranges: {
+    'Today': [moment(), moment()],
+    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+    'This Month': [moment().startOf('month'), moment().endOf('month')],
+    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+ },
   locale: {
     //format: 'M/DD hh:mm A'
     format: 'M/DD/YYYY'
@@ -286,3 +297,26 @@ $('.removeall').text('Hide All');
   });
   
 });
+
+
+    var start = moment().subtract(29, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
