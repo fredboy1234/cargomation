91022cb40bf02b8c8c43b464cf2fea3cffcf6737
johnalexladra <johnalexladra@gmail.com>
2021-01-22 12:58:32 +0800

    fixed: new loader has issue when scroll down

--- www/public_html/js/shipment.js
+++ www/public_html/js/shipment.js
@@ -81,8 +81,7 @@ $(document).ready(function(){
     // destroy: true,
     processing: true,
     language: {
-      // processing: '<center><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span></center>'
-      processing: loader
+      processing: '<center><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span></center>'
     },
     serverMethod: 'post',
     ajax: {
