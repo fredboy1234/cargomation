c1617c9fa18ae4e88307f2910a8825578226a455
a2bfred <71646332+a2bfred@users.noreply.github.com>
2021-02-23 15:28:26 +0800

    Update index.php

--- process_script/xml_mapper/data_mine/Wespac_ExchangerateAPI/index.php
+++ process_script/xml_mapper/data_mine/Wespac_ExchangerateAPI/index.php
@@ -1,14 +1,9 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
- 
- /*live exchange rate from westpac*/
- $url = "https://www.westpac.com.au/bin/getJsonRates.wbc.fxc.json";
- echo $json = file_get_contents($url);
-
-  
-  
+<?php 
+header(base64_decode('QWNjZXNzLUNvbnRyb2wtQWxsb3ctT3JpZ2luOiAq'));
+header(base64_decode('Q29udGVudC1UeXBlOiBhcHBsaWNhdGlvbi9qc29uOyBjaGFyc2V0PVVURi04'));
+header(base64_decode('QWNjZXNzLUNvbnRyb2wtQWxsb3ctTWV0aG9kczogT1BUSU9OUyxHRVQsUE9TVCxQVVQsREVMRVRF'));
+header(base64_decode('QWNjZXNzLUNvbnRyb2wtTWF4LUFnZTogMzYwMA=='));
+header(base64_decode('QWNjZXNzLUNvbnRyb2wtQWxsb3ctSGVhZGVyczogQ29udGVudC1UeXBlLCBBY2Nlc3MtQ29udHJvbC1BbGxvdy1IZWFkZXJzLCBBdXRob3JpemF0aW9uLCBYLVJlcXVlc3RlZC1XaXRo'));
+$c0=base64_decode('aHR0cHM6Ly93d3cud2VzdHBhYy5jb20uYXUvYmluL2dldEpzb25SYXRlcy53YmMuZnhjLmpzb24=');
+echo $n1=file_get_contents($c0);
 ?>
