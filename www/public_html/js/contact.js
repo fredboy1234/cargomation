67fcecf7354c90e57f9d78879e477f054691c659
John Alex Ladra <johnalexladra@Johns-MBP.lan>
2021-03-18 01:16:22 +0800

    update: contact page

--- www/public_html/js/contact.js
+++ www/public_html/js/contact.js
@@ -1,20 +1,30 @@
-$(document).ready(function(){
-    $("#contact-form").submit(function(e){
-       
+$(document).ready(function () {
+    $("#contact-form").submit(function (e) {
+
         var data = $(this).serializeArray();
         var email = data[0]['value'];
         var name = data[1]['value'];
         var message = data[2]['value'];
-       
+
         $.ajax({
-            url:'/contact/sendEmail',
+            url: '/contact/sendEmail',
             type: "POST",
-            dataType:"json",
-            data:{
+            dataType: "json",
+            data: {
                 'name': name,
                 'email': email,
                 'message': message
-            },success: function(result){
+            },
+            beforeSend: function () {
+                $("#contact-form .card-body").append('<center id="loader"><i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span></center>');
+            }, success: function (result) {
+                $('#loader').remove();
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Message Sent!',
+                    text: 'Your message was sent!',
+                    timer: 3000
+                });
                 console.log(result);
             }
         });
