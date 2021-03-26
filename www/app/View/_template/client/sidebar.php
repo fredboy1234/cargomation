b00db32d89deb0d485aff1d000b92f95fc7937b3
John Alex Ladra <johnalexladra@gmail.com>
2021-03-27 02:56:40 +0800

    update: minor UI updates

--- www/app/View/_template/client/sidebar.php
+++ www/app/View/_template/client/sidebar.php
@@ -32,11 +32,11 @@ function isActive($url) {
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <!-- <a href="/" class="brand-link">
+    <a href="/" class="brand-link">
       <img src="/bower_components/admin-lte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">A2B Freight Hub</span>
-    </a> -->
+    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
@@ -133,9 +133,4 @@ function isActive($url) {
       <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
-    <a href="/" class="brand-link">
-      <img src="/bower_components/admin-lte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light"><?=APP_NAME?></span>
-    </a>
   </aside>
