dce44f5c52f20361a90907ab40d329eb4a6bb58d
neil <neildesucatan@gmail.com>
2021-06-11 06:12:13 +0800

    Update: vessel remove all api request and use DB data

--- www/app/Controller/Vessel.php
+++ www/app/Controller/Vessel.php
@@ -206,7 +206,7 @@ class Vessel extends Core\Controller {
             "polyline" => $color_code_vessel,
             "c_flag" => $c_flag,
             "vesselnum" => $vessel_number,
-            "searatesTracking" => $_SESSION['searates'],
+            "searatesTracking" => $this->Vessel->getSearatesDB(),
             "tracking" => isset($_SESSION['tracking']) ? $_SESSION['tracking'] : '' 
         ]);
         $this->externalTemp();
@@ -287,7 +287,8 @@ class Vessel extends Core\Controller {
            'geocodeToken' => 'pk.fe49a0fae5b7f62ed12a17d8c2a77691',
            "notifications" => Model\User::getUserNotifications($user),
            "vesselyod" => $vloyds,
-           "searatesTracking" => isset($_SESSION['livesearates']) ? $_SESSION['livesearates'] : ''
+           "searatesTracking" => isset($_SESSION['livesearates']) ? $_SESSION['livesearates'] : '',
+           "searatesTracking" => $this->Vessel->getSearatesDB(),
        ]);
    }
 
