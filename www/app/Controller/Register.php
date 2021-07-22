9c327da66ceb3002a436b07a1d0494633d61aba4
John Alex Ladra <johnalexladra@gmail.com>
2021-07-23 01:50:44 +0800

    New Controller contact
    Updated
    _register

--- www/app/Controller/Register.php
+++ www/app/Controller/Register.php
@@ -83,6 +83,68 @@ class Register extends Core\Controller {
         ]);
     }
 
+    public function contact($contactID = "") {
+
+        // Check that the user is unauthenticated.
+        Utility\Auth::checkAuthenticated();
+
+        // Get an instance of the user model using the ID stored in the session. 
+        $userID = Utility\Session::get(Utility\Config::get("SESSION_USER"));
+        if (!$User = Model\User::getInstance($userID)) {
+            Utility\Redirect::to(APP_URL);
+        }        
+
+        if (!$Role = Model\Role::getInstance($userID)) {
+            Utility\Redirect::to(APP_URL);
+        }
+
+        $role = $Role->getUserRole($userID);
+
+        if(empty($role)) {
+            Utility\Redirect::to(APP_URL . $role);
+        }
+        
+        $selectedTheme = $User->getUserSettings($userID);
+        if(isset($selectedTheme[0]) && !empty($selectedTheme)){
+            $selectedTheme = $selectedTheme[0]->theme;
+        }else{
+            $selectedTheme = '';
+        }
+
+        $this->View->addCSS("css/theme/".$selectedTheme.".css");
+        $this->View->addCSS("css/".$selectedTheme.".css");
+       
+        // Render view template
+        // Usage renderTemplate(string|$template, string|$filepath, array|$data)
+
+        $imageList = (Object) Model\User::getProfile($userID);
+        $profileImage = '/img/default-profile.png';
+        foreach($imageList->user_image as $img){
+            if( $img->image_src!="" && $img->image_type=='profile' ){
+                $profileImage = base64_decode($img->image_src);
+            }
+        }
+
+        // Set any dependencies, data and render the view.
+        $this->View->addJS("js/register.js");
+
+        // Render view template
+        // Usage renderTemplate(string|$template, string|$filepath, array|$data)
+        $this->View->renderTemplate("register/index", [
+            "title" => "Register",
+            "data" => (new Presenter\Profile($User->data()))->present(),
+            "notifications" => Model\User::getUserNotifications($userID),
+            "user" => (Object) Model\User::getProfile($userID),
+            // "users" => Model\User::getUsersInstance($userID),
+            "contact_info" => $User->getUserContactInfo($contactID),
+            "image_profile" => $profileImage,
+            "dash_photo" =>Model\User::getUsersDashPhoto($userID),
+            "selected_theme" => $selectedTheme,
+            "role" => $role
+        ]);
+
+    }
+
     /**
      * Register: Processes a create account request. NOTE: This controller can
      * only be accessed by unauthenticated users!
@@ -91,8 +153,7 @@ class Register extends Core\Controller {
      * @return void
      * @since 1.0.2
      */
-    public function _register() {
-       
+    public function _register($contactID = "") {
         // Check that the user is unauthenticated.
         //Utility\Auth::checkUnauthenticated();
 
@@ -101,7 +162,7 @@ class Register extends Core\Controller {
         // Process the register request, redirecting to the login controller if
         // successful or back to the register controller if not.
         
-        if (Model\UserRegister::register($accId)) { 
+        if (Model\UserRegister::register($accId, $contactID)) { 
             Utility\Redirect::to(APP_URL . "login");
         }
        
