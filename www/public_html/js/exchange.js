f264e614dbb6b5708ad7660f9b80d04c0ad1975e
a2bfred <71646332+a2bfred@users.noreply.github.com>
2021-07-27 09:29:14 +0800

    Update Exchange Rate Sort Display

--- www/public_html/js/exchange.js
+++ www/public_html/js/exchange.js
@@ -21,12 +21,12 @@ $(function () {
     pageLength: 30,
     lengthChange: false,
     colReorder: true,
-    order: false,
+    order: [],
     columns: [
+       null,
        {orderable: false },
        {orderable: false },
-       {orderable: false },
-       {orderable: false },
+       null,
        {orderable: false }
     ]
   });
