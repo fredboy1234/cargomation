a2b23028919ba309872fb3ffa866ad7653cbf497
John Alex Ladra <johnalexladra@gmail.com>
2021-07-31 02:42:04 +0800

    add: created new fmodel function getShipmentByOrgCode(ORG_CODE, PARAMETERS)

--- www/app/Model/Shipment.php
+++ www/app/Model/Shipment.php
@@ -309,4 +309,13 @@ class Shipment extends Core\Model {
         from shipment where shipment.user_id = '{$user_id}' and port_loading is not null or port_loading <>' ' group by port_loading")->results();
     }
 
+    public static function getShipmentByOrgCode($org_code, $args = "*") {
+        $query = "SELECT {$args} FROM shipment ";
+        $query .= "LEFT JOIN shipment_assigned on shipment.id = shipment_assigned.shipment_id ";
+        $query .= "FULL OUTER JOIN Merge_Container on shipment.id = Merge_Container.[SHIPMENT ID] ";
+        $query .= "WHERE shipment.consignee = '{$org_code}' OR shipment.consignor = '{$org_code}'";
+        $Db = Utility\Database::getInstance();
+        return $Db->query($query)->results();
+    }
+
 }
