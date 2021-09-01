b80a312211cff15d248b2bbb1885ab91a8c15099
a2bfred <71646332+a2bfred@users.noreply.github.com>
2021-09-01 10:05:47 +0800

    Update: changed api source url

--- process_script/xml_mapper/data_mine/Wespac_ExchangerateAPI/execute_db.php
+++ process_script/xml_mapper/data_mine/Wespac_ExchangerateAPI/execute_db.php
@@ -4,7 +4,7 @@ require_once ('../../main/jsonpath-0.8.1.php');
 require_once ('../../main/json.php');
 
 
-$json = file_get_contents('http://a2bfreighthub.com/exchangerate/');
+$json = file_get_contents('https://www.cargomation.com/exchangerate/');
 $data = json_decode($json,true);
 $json_url = "$.data.Brands.WBC.Portfolios['Foreign Exchange Currencies'].Products";
 $curr_table = "dbo.currency";
