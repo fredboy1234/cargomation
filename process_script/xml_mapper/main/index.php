4dd32a404f3ad0905d3abfca0a0cca58ce875df3
a2bfred <71646332+a2bfred@users.noreply.github.com>
2021-09-30 11:40:32 +0800

    Update index.php

--- process_script/xml_mapper/main/index.php
+++ process_script/xml_mapper/main/index.php
@@ -657,7 +657,7 @@ try{
 					//GET DISCHARGE CODE
 					$XPATH_TRANSDISCHARGE = jsonPath($universal_shipment, $path_TransportLegCollection."[$k].PortOfDischarge.Name");
 					$XPATH_TRANSDISCHARGE = $parser->encode($XPATH_TRANSDISCHARGE);
-					$TRANSDISCHARGE = node_exist(getArrayName($XPATH_TRANSDISCHARGE));
+					$TRANSDISCHARGE = node_exist(getArrayName($XPATH_TRANSDISCHARGE)); 
 
 					$items[] = array("LegOrder"=>$LEG_ORDER,"LegType"=>$LEG_TYPE,"VesselName"=>$TRANSVESSELNAME,"Destination"=>$TRANSDISCHARGE);
 					
