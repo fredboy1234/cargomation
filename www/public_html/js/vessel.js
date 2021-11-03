f6211dacdb05db64dba56454952b0696563801e8
neil-De <neildesucatan@gmail.com>
2021-11-03 10:57:20 +0800

    update flter

--- www/public_html/js/vessel.js
+++ www/public_html/js/vessel.js
@@ -10,8 +10,20 @@ jQuery(document).ready(function() {
       }
       
     });
-    // console.log('test');
-    // console.log($('table tr').find('td'));
+    $('#datefilter').daterangepicker({
+      ranges: {
+          'Today': [moment(), moment()],
+          'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+          'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          'This Month': [moment().startOf('month'), moment().endOf('month')],
+          'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      },
+      startDate: moment(),
+  }, function(start, end, label) {
+      console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+  });
+
     $('table').on('mouseover','tr',function(e){
       if($(e.target).hasClass('mmatchhover')){
         $(this).css({"z-index":"100"});
