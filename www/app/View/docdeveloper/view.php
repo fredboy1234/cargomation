04c15946dae8ea2257a8967315d0b5057746d317
John Alex Ladra <johnalexladra@gmail.com>
2021-11-04 11:38:18 +0800

    update: view

--- www/app/View/docdeveloper/view.php
+++ www/app/View/docdeveloper/view.php
@@ -1,7 +1,19 @@
+<?php
+
+$file_src = "/document/fileviewer/" . $this->user_id . "/" . $this->doc_id;
+
+?>
 <div class="container">
   <div class="row">
     <div class="col-md-7">
-        <iframe src="https://cargomation.com/document/fileviewer/158/3376" width="100%" height="500px"></iframe>
+    	<?php
+    			/* Most browsers no longer support Java Applets and Plug-ins.
+    			   ActiveX controls are no longer supported in any browsers.
+				   The support for Shockwave Flash has also been turned off in modern browsers.
+				   You may use "iframe" or any other third party plugins
+    			*/ 
+		  ?>
+        <embed src="<?= $file_src; ?>" width="100%" height="500px">
     </div>
     <div class="col-md-5 bg-primary">
             
