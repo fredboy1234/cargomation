ce07ae4d66c343a6b7b809d632ae36c8c8ca092a
John Alex Ladra <johnalexladra@gmail.com>
2021-11-05 11:07:58 +0800

    test dropdown

--- www/app/View/docdeveloper/view.php
+++ www/app/View/docdeveloper/view.php
@@ -37,19 +37,54 @@ $file_src = "/document/fileviewer/" . $this->user_id . "/" . $this->doc_id;
         </div>
         <div class="card-footer p-0">
           <ul class="nav flex-column">
-            <?php 
-              foreach ($this->file_stat->files[0]->rank as $key => $value):         
-            ?>
+            <?php foreach ($this->file_stat->files[0]->rank as $key => $value): ?>
             <li class="nav-item">
-              <a href="#" class="nav-link">
-              <?= strtoupper($key); ?> <span class="float-right badge bg-primary"><?= number_format(round($value, 2), 2); ?></span>
-              </a>
+              <span href="#" class="nav-link">
+              <?= strtoupper($key); ?> <span class="float-right"><?= number_format(round($value, 2), 2); ?></span>
+              </span>
             </li>
             <?php endforeach; ?>
           </ul>
         </div>
       </div>
     </div>
+  </div>
+  <div class="row">
+  <div class="card card-info">
+    <div class="card-header">
+      <h3 class="card-title">File Trainer</h3>
+    </div>
+    <!-- /.card-header -->
+    <!-- form start -->
+    <form class="form-horizontal">
+      <div class="card-body">
+        <div class="form-group row">
+          <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+          <div class="col-sm-10">
+            <select name="type" id="type" class="form-control">
+                <option value="" selected="" disabled="" hidden="">Choose file type</option>
+                <optgroup label="Default type" data-option="default">
+                <?php foreach ($this->file_stat->files[0]->rank as $key => $value): ?>
+                  <option value="<?= strtoupper($key); ?> ">
+                    <?= strtoupper($key); ?> - <?= number_format(round($value, 2), 2); ?>
+                  </option>
+                <?php endforeach; ?>
+                </optgroup>
+                <optgroup label="All file type" data-option="all">
+                    <option value="CIV">CIV</option>
+                    <option value="MSC">MSC</option>
+                </optgroup>
+            </select>
+          </div>
+        </div>
+      </div>
+      <!-- /.card-body -->
+      <div class="card-footer">
+        <button type="submit" class="btn btn-info float-right">Sign in</button>
+      </div>
+      <!-- /.card-footer -->
+    </form>
+  </div>
   </div>
 </div>
 
