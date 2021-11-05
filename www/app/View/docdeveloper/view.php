09d67332e7554b18005751a5e6b54ef57e49e59c
John Alex Ladra <johnalexladra@gmail.com>
2021-11-05 11:37:35 +0800

    update: add all doc type by user

--- www/app/View/docdeveloper/view.php
+++ www/app/View/docdeveloper/view.php
@@ -47,44 +47,45 @@ $file_src = "/document/fileviewer/" . $this->user_id . "/" . $this->doc_id;
           </ul>
         </div>
       </div>
-    </div>
-  </div>
-  <div class="row">
-  <div class="card card-info">
-    <div class="card-header">
-      <h3 class="card-title">File Trainer</h3>
-    </div>
-    <!-- /.card-header -->
-    <!-- form start -->
-    <form class="form-horizontal">
-      <div class="card-body">
-        <div class="form-group row">
-          <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-          <div class="col-sm-10">
-            <select name="type" id="type" class="form-control">
-                <option value="" selected="" disabled="" hidden="">Choose file type</option>
-                <optgroup label="Default type" data-option="default">
-                <?php foreach ($this->file_stat->files[0]->rank as $key => $value): ?>
-                  <option value="<?= strtoupper($key); ?> ">
-                    <?= strtoupper($key); ?> - <?= number_format(round($value, 2), 2); ?>
-                  </option>
-                <?php endforeach; ?>
-                </optgroup>
-                <optgroup label="All file type" data-option="all">
-                    <option value="CIV">CIV</option>
-                    <option value="MSC">MSC</option>
-                </optgroup>
-            </select>
-          </div>
+      <div class="card card-info">
+        <div class="card-header">
+          <h3 class="card-title">File Trainer</h3>
         </div>
+        <!-- /.card-header -->
+        <!-- form start -->
+        <form class="form-horizontal">
+          <div class="card-body">
+            <div class="form-group row">
+              <label for="inputEmail3" class="col-sm-2 col-form-label">Type</label>
+              <div class="col-sm-10">
+                <select name="type" id="type" class="form-control">
+                    <option value="" selected="" disabled="" hidden="">Choose file type</option>
+                    <optgroup label="Default type" data-option="default">
+                    <?php foreach ($this->file_stat->files[0]->rank as $key => $value): ?>
+                      <option value="<?= strtoupper($key); ?> ">
+                        <?= strtoupper($key); ?> (<?= number_format(round($value, 2), 2); ?>)
+                      </option>
+                    <?php endforeach; ?>
+                    </optgroup>
+                    <optgroup label="All file type" data-option="all">
+                      <?php foreach ($this->doc_type as $key => $value): ?>
+                      <option value="<?= strtoupper($value->type); ?>">
+                        <?= strtoupper($value->type); ?>
+                      </option>
+                      <?php endforeach; ?>
+                    </optgroup>
+                </select>
+              </div>
+            </div>
+          </div>
+          <!-- /.card-body -->
+          <div class="card-footer">
+            <button type="submit" class="btn btn-info float-right">Train</button>
+          </div>
+          <!-- /.card-footer -->
+        </form>
       </div>
-      <!-- /.card-body -->
-      <div class="card-footer">
-        <button type="submit" class="btn btn-info float-right">Sign in</button>
-      </div>
-      <!-- /.card-footer -->
-    </form>
-  </div>
+    </div>
   </div>
 </div>
 
