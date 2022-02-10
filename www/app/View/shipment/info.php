34bfde6894d7de12ddf13f513ea38ae750218a85
John Alex Ladra <johnalexladra@gmail.com>
2022-02-10 09:20:05 +0800

    update shipment info

--- www/app/View/shipment/info.php
+++ www/app/View/shipment/info.php
@@ -1,8 +1,6 @@
-
-
 <div class="row">
     <div class="col-lg-3">
-         <h1> <?= $this->shipment_info[0]->shipment_num; ?></h1>    
+         <h1 class="p-3"> <?= $this->shipment_info[0]->shipment_num; ?></h1>    
     </div>
     <div class="col-lg-9">
         <div>
@@ -74,18 +72,6 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-3">
-                    <strong>Transport Mode</strong>
-                    <p><?= $this->shipment_info[0]->transport_mode; ?></p>
-                </div>
-                <div class="col-lg-9">
-                    <strong>Estimated Arrival</strong>
-                    <p><?php 
-                        $date = date_create( $this->shipment_info[0]->eta);
-                        echo date_format($date,"F d, Y H:i:s"); ?></p>
-                </div>
-            </div>
-            <div class="row">
                 <div class="col-lg-4">
                     <strong>Voyage Flight No.:</strong>
                     <p><?= $this->shipment_info[0]->voyage_flight_num; ?></p>
@@ -100,60 +86,83 @@
                 </div>
             </div>
             <div class="row">
-                <div class="w-100 p-1" style="background-color: #eee;">
-                    <h5 class="d-inline-block">Consignee</h5>
-                    <span class="float-right"><?= $this->shipment_info[0]->consignee; ?></span>
+                <div class="col-lg-4">
+                    <strong>Transport Mode</strong>
+                    <p><?= $this->shipment_info[0]->transport_mode; ?></p>
                 </div>
-                <div class="col-lg-12">
-                    <strong>Address:</strong>
-                    <p><?= $this->shipment_info[0]->consignee_addr; ?></p>
+                <div class="col-lg-3">
+                    <strong>Departured</strong>
+                    <p><?php 
+                        $date = date_create( $this->shipment_info[0]->etd);
+                        echo date_format($date,"d/m/Y"); ?></p>
                 </div>
-                <div class="col-lg-12">
-                    <strong>Sending Agent:</strong>
-                    <p><?= $this->shipment_info[0]->sending_agent; ?></p>
-                </div>
-                <div class="col-lg-12">
-                    <strong>Sending Agent Address:</strong>
-                    <p><?= $this->shipment_info[0]->sending_agent_addr; ?></p>
-                </div>
-                <div class="col-lg-6">
-                    <strong>Delivery Place:</strong>
-                    <p><?= $this->shipment_info[0]->place_delivery; ?></p>
-                </div>
-                <div class="col-lg-6">
-                    <strong>Port of Loading: </strong>
-                    <p><?= $this->shipment_info[0]->port_loading; ?></p>
+                <div class="col-lg-5">
+                    <strong>Estimated Arrival</strong>
+                    <p><?php 
+                        $date = date_create( $this->shipment_info[0]->eta);
+                        echo date_format($date,"F d, Y H:i:s"); ?></p>
                 </div>
             </div>
-            <div class="row">
-                <div class="w-100 p-1" style="background-color: #eee;">
-                    <h5 class="d-inline-block">Consignor</h5>
-                    <span class="float-right"><?= $this->shipment_info[0]->consignor; ?></span>
+            <div class="w-100 p-2 mb-2" style="background-color: #eee;" data-toggle="collapse" data-target="#consignee" aria-expanded="true">
+                <h5 class="d-inline-block">Consignee</h5>
+                <span class="float-right"><?= $this->shipment_info[0]->consignee; ?></span>
+            </div>
+            <dl id="consignee" class="row collapse show">
+                <div class="col-lg-3">
+                    <dt>Code:</dt>
+                    <dd><?= $this->shipment_info[0]->consignee; ?></dd>
+                </div>
+                <div class="col-lg-9">
+                    <dt>Address:</dt>
+                    <dd><?= $this->shipment_info[0]->consignee_addr; ?></dd>
                 </div>
                 <div class="col-lg-12">
-                    <strong>Address:</strong>
-                    <p><?= $this->shipment_info[0]->consignor_addr; ?></p>
+                    <dt>Sending Agent:</dt>
+                    <dd><?= $this->shipment_info[0]->sending_agent; ?></dd>
                 </div>
                 <div class="col-lg-12">
-                    <strong>Receiving Agent:</strong>
-                    <p><?= $this->shipment_info[0]->receiving_agent; ?></p>
-                </div>
-                <div class="col-lg-12">
-                    <strong>Receiving Agent Address:</strong>
-                    <p><?= $this->shipment_info[0]->receiving_agent_addr; ?></p>
+                    <dt>Sending Agent Address:</dt>
+                    <dd><?= $this->shipment_info[0]->sending_agent_addr; ?></dd>
                 </div>
                 <div class="col-lg-6">
-                    <strong>Receipt Place:</strong>
-                    <p><?= $this->shipment_info[0]->place_receipt; ?></p>
+                    <dt>Delivery Place:</dt>
+                    <dd><?= $this->shipment_info[0]->place_delivery; ?></dd>
                 </div>
                 <div class="col-lg-6">
-                    <strong>Port of Discharge: </strong>
-                    <p><?= $this->shipment_info[0]->port_discharge; ?></p>
+                    <dt>Port of Loading: </dt>
+                    <dd><?= $this->shipment_info[0]->port_loading; ?></dd>
                 </div>
+            </dl>
+            <div class="w-100 p-2 mb-2" style="background-color: #eee;" data-toggle="collapse" data-target="#consignor" aria-expanded="true" >
+                <h5 class="d-inline-block">Consignor</h5>
+                <span class="float-right"><?= $this->shipment_info[0]->consignor; ?></span>
             </div>
-            <div class="row">
-                <p><b>ETD: </b><span><?= $this->shipment_info[0]->etd; ?></span></p>
-            </div>
+            <dl id="consignor" class="row collapse">
+                <div class="col-lg-3">
+                    <dt>Code:</dt>
+                    <dd><?= $this->shipment_info[0]->consignor; ?></dd>
+                </div>
+                <div class="col-lg-9">
+                    <dt>Address:</dt>
+                    <dd><?= $this->shipment_info[0]->consignor_addr; ?></dd>
+                </div>
+                <div class="col-lg-12">
+                    <dt>Receiving Agent:</dt>
+                    <dd><?= $this->shipment_info[0]->receiving_agent; ?></dd>
+                </div>
+                <div class="col-lg-12">
+                    <dt>Receiving Agent Address:</dt>
+                    <dd><?= $this->shipment_info[0]->receiving_agent_addr; ?></dd>
+                </div>
+                <div class="col-lg-6">
+                    <dt>Receipt Place:</dt>
+                    <dd><?= $this->shipment_info[0]->place_receipt; ?></dd>
+                </div>
+                <div class="col-lg-6">
+                    <dt>Port of Discharge: </dt>
+                    <dd><?= $this->shipment_info[0]->port_discharge; ?></dd>
+                </div>
+            </dl>
         </div>
     </section>
     <section class="col-lg-6 connectedSortable ui-sortable">
@@ -171,14 +180,139 @@
         </div>
         <div class="card-body">
             <div class="row">
-                <div class="col-md-6">
-                    [INSERT MAP HERE]
+                <div class="col-md-12">
+                    <div id="chartdiv" class="" style="position: relative; height: 300px;"></div>
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-12">
+                <div id="accordion" class="mt-2">
+                    <?php if(true):
+                        $json = json_decode($this->shipment_info[0]->route_leg);
+                        foreach ($json as $key => $value):
+                    ?>
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                            <h5 class="mb-0">
+                            <div class="container btn btn-link" data-toggle="collapse" data-target="#collapse<?= $value->LegOrder; ?>" aria-expanded="true" aria-controls="collapse<?= $value->LegOrder; ?>">
+                                    <div class="row">
+                                        <div class="col-sm-5"><?php echo $value->Origin; ?></div>
+                                        <div class="col-sm-2 text-center"><i class="fas fa-arrow-right"></i></div>
+                                        <div class="col-sm-5"><?php echo $value->Destination; ?></div>
+                                    </div>
+                                </div>
+                            </h5>
+                        </div>
+
+                        <div id="collapse<?= $value->LegOrder; ?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <strong>Shipment Leg </strong>
+                                        <p><?= $value->LegOrder; ?> of <?= count($json) ?></p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <strong>Origin Port</strong>
+                                        <p><?=  $value->Origin ?></p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <strong>Destination Port</strong>
+                                        <p><?=  $value->Destination ?></p>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-4">
+                                        <strong>Vessel Name</strong>
+                                        <p><?= $value->VesselName; ?></p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <strong>Type</strong>
+                                        <p><?= $value->LegType; ?></p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                        <strong>Status</strong>
+                                        <p><?=  $value->BookingStatus ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                    <?php endif; ?>
                 </div>
+                </div>
+            </div>
+            <div class="row">
+
             </div>
         </div>
     </section>
 </div>
 
 </div>
+<style>
+.amcharts-myline path {
+  stroke-linejoin: round;
+  stroke-linecap: round;
+  stroke-dasharray: 500%;
+  stroke-dasharray: 0.25rem; /* fixes IE prob */
+  stroke-dashoffset: 0; /* fixes IE prob */
+  -webkit-animation: am-draw 100s;
+  animation: am-draw 100s;
+}
+@-webkit-keyframes am-draw {
+  0% {
+    stroke-dashoffset: 500%;
+  }
+  100% {
+    stroke-dashoffset: 0%;
+  }
+}
+@keyframes am-draw {
+  0% {
+    stroke-dashoffset: 500%;
+  }
+  100% {
+    stroke-dashoffset: 0%;
+  }
+}
+</style>
+<script src="/js/map.js"></script>
+<script>
+var route = JSON.parse(<?= json_encode($this->shipment_info[0]->route_leg) ?>);
+$(document).ready(function() {  
+
+    var pointLeg = [];
+    var pointObject = [];
+    var newObject = [];
+
+    $.each(route, function(key, value) {
+        pointLeg.push({
+            "order": value.LegOrder,
+            "point": value.Origin
+        });
+        pointLeg.push({
+            "order": value.LegOrder,
+            "point": value.Destination
+        });
+    });
+
+    $.each(pointLeg, function(key, value) {
+        $.get('https://maps.googleapis.com/maps/api/geocode/json?address='+value.point+'&key=AIzaSyA89i4Tuzrby4Dg-ZxnelPs-U3uvHoR9eo', function(data){ 
+            if(data.status === 'OK') {
+                var latitude = data.results[0].geometry.location.lat;
+                var longitude = data.results[0].geometry.location.lng;
+                pointObject.push({
+                    "latitude": latitude,
+                    "longitude": longitude,
+                    "title": value.point,
+                    "order": value.order
+                });
+            }
+        });
+    });
+
+    console.log(pointObject);
+
+    let map = new simpleMap(pointObject, "chartdiv");
+    map.executeMap();
+});
+</script>
