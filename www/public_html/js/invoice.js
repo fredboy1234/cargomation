5b758e1016eccd1fed9a4c12ad061cf4ea747f16
John Alex Ladra <johnalexladra@gmail.com>
2022-02-11 15:04:37 +0800

    updated invoice, fix bugs

--- www/public_html/js/invoice.js
+++ www/public_html/js/invoice.js
@@ -1,14 +1,28 @@
-    // Bootstrap File Input
-    $("#invoice").fileinput({
-        theme: 'fas',
-        uploadUrl: "/invoice/uploadDocument/" + user_id,
-        uploadAsync: false,
-        minFileCount: 1,
-        maxFileCount: 20,
-        overwriteInitial: false,
-        initialPreviewAsData: true, // identify if you are sending preview data only and not the raw markup
-    }).on('filesorted', function(e, params) {
-        console.log('File sorted params', params);
-    }).on('fileuploaded', function(e, params) {
-        console.log('File uploaded params', params);
-    });
+// Bootstrap File Input
+$("#invoice").fileinput({
+    theme: 'fas',
+    uploadUrl: "/invoice/upload/" + user_id,
+    deleteUrl: "/invoice/delete/" + user_id,
+    enableResumableUpload: true,
+    resumableUploadOptions: {
+        // uncomment below if you wish to test the file for previous partial uploaded chunks
+        // to the server and resume uploads from that point afterwards
+        // testUrl: "http://localhost/test-upload.php"
+    },
+    uploadExtraData: {
+        'uploadToken': token, // for access control / security 
+    },
+    maxFileCount: 20,
+    // allowedFileTypes: ['docx'],    // allow only images
+    showCancel: true,
+    initialPreviewAsData: true,
+    overwriteInitial: false,
+    // initialPreview: [],          // if you have previously uploaded preview files
+    // initialPreviewConfig: [],    // if you have previously uploaded preview files
+}).on('fileuploaded', function(event, previewId, index, fileId) {
+    console.log('File Uploaded', 'ID: ' + fileId + ', Thumb ID: ' + previewId);
+}).on('fileuploaderror', function(event, data, msg) {
+    console.log('File Upload Error', 'ID: ' + data.fileId + ', Thumb ID: ' + data.previewId);
+}).on('filebatchuploadcomplete', function(event, preview, config, tags, extraData) {
+    console.log('File Batch Uploaded', preview, config, tags, extraData);
+});
