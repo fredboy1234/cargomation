a0391b8a623289f4404c3aab9f4d821c7b0257f9
a2bfred <71646332+a2bfred@users.noreply.github.com>
2022-03-17 22:49:25 +0800

    Update: fixed and revamp edi link
    issue with its loading time to mail server

--- process_script/xml_mapper/main/mail.php
+++ process_script/xml_mapper/main/mail.php
@@ -21,7 +21,7 @@ $var_code = "license_code";
 $var_col = "(shipment_num,master_bill,house_bill,shipper,client,macro_link,license_code)";
 
 //The location of the mailbox.
-$mailbox = base64_decode('e2ltYXAuc2VjdXJlc2VydmVyLm5ldDo5OTUvcG9wMy9zc2wvbm92YWxpZGF0ZS1jZXJ0fQ==');
+$mailbox = "{imap.secureserver.net:995/pop3/ssl/novalidate-cert}INBOX";
 //The username / email address that we want to login to.
 $username = $email;
 //The password for this email address.
@@ -30,17 +30,20 @@ $password = $pass;
 
 //Attempt to connect using the imap_open function.
 try{
+$start = microtime(true); // start timer	
 $imapResource = imap_open($mailbox, $username, $password);
+
 }
 catch (Exception $e) {
     echo 'Caught exception: ',  $e->getMessage(), "\n";
 }
- 
+
 //If the imap_open function returns a boolean FALSE value,
 //then we failed to connect.
 if($imapResource === false){
     //If it failed, throw an exception that contains
     //the last imap error.
+
     throw new Exception(imap_last_error());
 } 
 
@@ -56,9 +59,9 @@ function get_data($string, $start, $end){
 
 //If we get to this point, it means that we have successfully
 //connected to our mailbox via IMAP.
-  
+
 //Lets get all emails that were received since a given date.
-$search = 'SINCE "' . date("j F Y", strtotime("-".$duration." day")) . '"';
+echo $search = 'SINCE "' . date("j F Y", strtotime("-".$duration." day")) . '"';
 $emails = imap_search($imapResource, $search);
 //$emails = imap_search($imapResource, "UNSEEN", SE_UID);
  
@@ -68,60 +71,65 @@ $ctr = 0;
 if(!empty($emails)){ 
     ##Loop through the emails.
     foreach($emails as $key=>$email){
-        ##Fetch an overview of the email.
-        $overview = imap_fetch_overview($imapResource, $email);
+		$overview = imap_fetch_overview($imapResource, $email);
         $overview = $overview[0]; 
-		##filter subject from cargowise Trigger Point
-		if(strpos(htmlentities($overview->subject),'Shipment - HTML to Hub') !== false){
-			
-        ##Print out the subject of the email.
-        //echo '<b>' . htmlentities($overview->subject) . '</b><br>';
-		
-        ##Print out the sender's email address / from email address.
-        //echo 'From: ' . $overview->from . '<br><br>';
-		
-        ##Get the body of the email using UTF-8 encode.
-        $message = imap_fetchbody($imapResource, $email, 1);
+			if(strpos(htmlentities($overview->subject),'Shipment - HTML to Hub') !== false){
+					$message = imap_fetchbody($imapResource, $email, 1);
+					if($username == 'tcf@cargomation.com'){
+						$tagLT = "&lt";
+						$tagGT = "&gt";
+						$tagSpan = '<s/span>';
+						$tempID = 'TCFSYDSYD';
+					}
+					elseif($username == 'imageinternational@cargomation.com'){
+						$tagLT = "&lt;";
+						$tagGT = "&gt;";
+						$tagSpan = '<s span="">';
+						$tempID = 'IM0SYDSYD';
+					}  
+						 $message = trim(strval($message));
+						 $job = trim(strval(get_data($message, 'ship</span><span>'.$tagGT.''.$tagSpan.'</pre>', '<span>'.$tagLT.'</span><span>/</span><span>ship')));
+						 $shipper =  trim(strval(get_data($message, 'shipper</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>shipper')));		
+						 $client =   trim(strval(get_data($message, 'client</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>client')));	
+						 $mbill = 	trim(strval(get_data($message, 'mbill</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>mbill')));		
+						 $hbill = 	trim(strval(get_data($message, 'hbill</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>hbill')));	
+						 $mcode = 	trim(strval(get_data($message, 'macro</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>macro')));
+						 $code =     trim(strval(str_replace('&amp;', '&', $mcode)));
+						 $lcode =	trim(strval(get_data($code, 'LicenceCode=', '&ControllerID')));
+						 
+					$new_array[] = array("shipment"=>$job, "shipper"=>$shipper,"client"=>$client,"mbill"=>$mbill,"hbill"=>$hbill,"mcode"=>$mcode,"code"=>$code,"lcode"=>$lcode);
 
-
-        //echo $message = quoted_printable_encode($message);
-
-        if($username == 'tcf@cargomation.com'){
-        	$tagLT = "&lt";
-        	$tagGT = "&gt";
-        	$tagSpan = '<s/span>';
-        }
-        elseif($username == 'imageinternational@cargomation.com'){
-        	$tagLT = "&lt;";
-        	$tagGT = "&gt;";
-        	$tagSpan = '<s span="">';
-        }  
-
-         $message = trim(strval($message));
-         //ship</span><span>&gt<s/span></pre>
-         //<span=>&lt</span><span>/</span><span>ship
-
-	     $job = trim(strval(get_data($message, 'ship</span><span>'.$tagGT.''.$tagSpan.'</pre>', '<span>'.$tagLT.'</span><span>/</span><span>ship')));		
-		 $shipper =  trim(strval(get_data($message, 'shipper</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>shipper')));		
-		 $client =   trim(strval(get_data($message, 'client</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>client')));	
-		 $mbill = 	trim(strval(get_data($message, 'mbill</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>mbill')));		
-		 $hbill = 	trim(strval(get_data($message, 'hbill</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>hbill')));	
-	     $mcode = 	trim(strval(get_data($message, 'macro</span><span>'.$tagGT.'</span>', '<span>'.$tagLT.'</span><span>/</span><span>macro')));
-		 $code =     trim(strval(str_replace('&amp;', '&', $mcode)));
-		 $lcode =	trim(strval(get_data($code, 'LicenceCode=', '&ControllerID')));
-		
-		$sql = "Select * from {$link_table} WHERE {$var_ship} LIKE '%{$job}%' AND {$var_code} = '{$lcode}'";
-		$execute_query = sqlsrv_query($conn, $sql);
-		$result = sqlsrv_has_rows($execute_query);
-		
-		if ($result === false && trim($mcode) != "") {
-		$sql = "Insert into {$link_table} {$var_col} values('{$job}','{$mbill}','{$hbill}','{$shipper}','{$client}','{$code}','{$lcode}')";
-		$execute_query = sqlsrv_query($conn, $sql);	
-		$ctr++;
 			}
-		}
     }
 }
+		$sql = "Select * from {$link_table} WHERE {$var_code} = '{$tempID}'";
+		$execute_query = sqlsrv_query($conn, $sql);
+		while ($row = sqlsrv_fetch_array($execute_query, SQLSRV_FETCH_ASSOC)) {
+			$ship_array[]  = $row['shipment_num'];
+		}
+		$insert_link = "INSERT INTO {$link_table} VALUES ";
+		$json_array = json_decode(json_encode ($new_array));
+		$ship_insert = "";
+		$value_query = "";
+		for ($i = 0; $i < count($new_array); $i++) {
+			 if(!in_array($json_array[$i]->shipment,$ship_array)){
+				 if(count($new_array) == 1){
+				 $ship_insert .= "('{$json_array[$i]->shipment}','{$json_array[$i]->mbill}','{$json_array[$i]->hbill}','{$json_array[$i]->shipper}','{$client}','{$json_array[$i]->code}','{$json_array[$i]->lcode}')";
+				 }
+				 else{
+				 $ship_insert .= "('{$json_array[$i]->shipment}','{$json_array[$i]->mbill}','{$json_array[$i]->hbill}','{$json_array[$i]->shipper}','{$client}','{$json_array[$i]->code}','{$json_array[$i]->lcode}'),";		
+				 }
+				 $ctr++;
+			 }
+		}
+		if(substr($ship_insert, -1) == ","){
+			$value_query = $insert_link.substr($ship_insert, 0, -1);
+		}
+		if(!empty($value_query)){
+			sqlsrv_query($conn,$value_query);
+		}
+
+echo 'connection: '.(microtime(true)-$start).' seconds'."\r\n"; // show results
 echo "Total Processed: ".$ctr;
 imap_close($imapResource);
 
