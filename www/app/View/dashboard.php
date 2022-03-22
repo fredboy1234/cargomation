9547d1f09d4197ab4b344b4ebd74538a843d5615
neil-De <neildesucatan@gmail.com>
2022-03-22 11:03:47 +0800

    update

--- www/app/View/dashboard.php
+++ www/app/View/dashboard.php
@@ -131,7 +131,8 @@
               <div class="icon">
                 <i class="fas fa-hourglass-end text-warning"></i>
               </div>
-              <a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <!-- <a href="#" class="small-box-footer text-dark">More info <i class="fas fa-arrow-circle-right"></i></a> -->
+              <a href="#" class="small-box-footer text-dark">Coming Soon</i></a>
             </div>
           </div>
           <!-- ./col -->
