7fbcfacd9ee798a18936232931a3c9349f01d1e4
a2bfred <71646332+a2bfred@users.noreply.github.com>
2022-03-28 12:44:23 +0800

    Add: vessel tracker controller

---
+++ www/app/Controller/vesseltracker.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Controller;
+
+use App\Core;
+use App\Model;
+use App\Utility;
+use App\Presenter;
+
+/**
+ * Docdeveloper Controller:
+ */
+
+class VesselTracker extends Core\Controller {
+
+    /**
+     * Order Index: Renders the order view. NOTE: This controller can only be accessed
+     * by authenticated users!
+     * @access public
+     * @example index/index
+     * @return void
+     * @since 1.0
+     */
+    public function index($user = "") {
+        // Check that the user is authenticated.
+        Utility\Auth::checkAuthenticated();
+        // If no user ID has been passed, and a user session exists, display
+        // the authenticated users profile.
+        if (!$user) {
+            $userSession = Utility\Config::get("SESSION_USER");
+            if (Utility\Session::exists($userSession)) {
+                $user = Utility\Session::get($userSession);
+            }
+        }
+        // // Get an instance of the user model using the user ID passed to the
+        // // controll action. 
+        if (!$User = Model\User::getInstance($user)) {
+            Utility\Redirect::to(APP_URL);
+        }
+
+        // Get an instance of the user role
+        if (!$Role = Model\Role::getInstance($user)) {
+            Utility\Redirect::to(APP_URL);
+        }
+
+        $role = $Role->getUserRole($user);
+
+        if(empty($role)) {
+            Utility\Redirect::to(APP_URL . $role);
+        }
+
+
+        if($role->role_id > 2) {
+            $sub_account = $User->getSubAccountInfo($user);
+            $user_key = $sub_account[0]->account_id;
+        } else {
+            $user_key = $user;
+        }
+
+        $selectedTheme = $User->getUserSettings($user);
+        
+        if(isset( $selectedTheme[0]) && !empty($selectedTheme)){
+            $selectedTheme = $selectedTheme[0]->theme;
+        }else{
+            $selectedTheme = 'default';
+        }
+
+
+        $this->View->addCSS("css/theme/".$selectedTheme.".css");
+        $this->View->addCSS("css/".$selectedTheme.".css");
+        $this->View->addJS("js/vesseltracker.js");
+
+        $imageList = (Object) Model\User::getProfile($user);
+        $profileImage = '/img/default-profile.png';
+        foreach($imageList->user_image as $img){
+            if( $img->image_src!="" && $img->image_type=='profile' ){
+                $profileImage = base64_decode($img->image_src);
+            }
+        }
+
+        $this->View->renderTemplate("/vesseltracker/index", [
+            "title" => "Vessel Tracker",
+            "data" => (new Presenter\Profile($User->data()))->present(),
+            "user" => (Object) Model\User::getProfile($user),
+            "notifications" => Model\User::getUserNotifications($user),
+            "menu" => Model\User::getUserMenu($role->role_id),
+            "image_profile" => $profileImage,
+            'role' => $role, 
+            'user_id' => $user,
+            'selected_theme' => $selectedTheme,
+            "user_settings" =>$User->defaultSettings($user_key, $role->role_id),
+            "settings_user" => $User->getUserSettings($user),
+        ]);
+    }
+}
