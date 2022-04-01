40693db83575be174a248f97cd185398297f4644
a2bfred <71646332+a2bfred@users.noreply.github.com>
2022-04-01 12:29:11 +0800

    Update: add cookie notif / privacy policy

--- www/app/View/_template/footer.php
+++ www/app/View/_template/footer.php
@@ -126,6 +126,18 @@ body {font-family: Arial, Helvetica, sans-serif;}
 <?php endif; ?>
 
 
+<!-- cookie card-->
+<div class="card cookie-alert">
+  <div class="card-body">
+    <p class="card-text">We use cookies to ensure you get the best experience on our website.</p>
+    <div class="btn-toolbar justify-content-end">
+      <a href="https://cargomation.com/" target="_blank" class="btn btn-link">Learn more</a>
+      <a href="#" class="btn btn-primary accept-cookies">Accept</a>
+    </div>
+  </div>
+</div>
+<!-- end cookie card-->
+
   <!-- /.content-wrapper -->
   <footer class="main-footer">
     <strong>Copyright &copy; 2019-2020 <a href="/"><?=APP_NAME?></a>.</strong>
@@ -244,6 +256,47 @@ crossorigin=""></script>
   CG.chatsubmit();
   });
   
+  (function () {
+    "use strict";
+
+    var cookieAlert = document.querySelector(".cookie-alert");
+    var acceptCookies = document.querySelector(".accept-cookies");
+
+    cookieAlert.offsetHeight; 
+
+    if (!getCookie("acceptCookies")) {
+        cookieAlert.classList.add("show");
+    }
+
+    acceptCookies.addEventListener("click", function () {
+        setCookie("acceptCookies", true, 60);
+        cookieAlert.classList.remove("show");
+        });
+    })();
+
+  // Cookie functions stolen from w3schools
+  function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(';');
+      for (var i = 0; i < ca.length; i++) {
+          var c = ca[i];
+          while (c.charAt(0) === ' ') {
+              c = c.substring(1);
+          }
+          if (c.indexOf(name) === 0) {
+              return c.substring(name.length, c.length);
+          }
+      }
+      return "";
+  }
 
 </script>
 <script src="/js/map.js"></script>
