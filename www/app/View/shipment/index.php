8bdfde63354cd46bba8121acddd6c3322b5cb387
John Alex <johnalexladra@gmail.com>
2022-04-05 08:05:21 +0800

    minor update

--- www/app/View/shipment/index.php
+++ www/app/View/shipment/index.php
@@ -244,7 +244,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Shipment</h3>
+                        <h3 class="card-title"></h3>
 
                         <div class="card-tools">
                             <div class="input-group input-group-sm" style="width: 150px;">
