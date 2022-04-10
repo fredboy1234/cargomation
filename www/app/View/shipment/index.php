3bbef0f2fc16b6df597881c0f726d01777726feb
John Alex Ladra <johnalexladra@gmail.com>
2022-04-10 12:20:09 +0800

    loader

--- www/app/View/shipment/index.php
+++ www/app/View/shipment/index.php
@@ -311,7 +311,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <div id="loader-wrapper" class="d-flex justify-content-center">
+                <div id="loader-wrapper2" class="d-flex justify-content-center">
                     <div class="spinner-border" role="status">
                         <span class="sr-only">Loading...</span>
                     </div>
