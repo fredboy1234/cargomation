be23a6a1548c3eadeb2536a663312a371f9936df
neil-De <neildesucatan@gmail.com>
2022-05-17 14:09:51 +0800

    DASJMAP

--- www/app/Controller/Dashboard.php
+++ www/app/Controller/Dashboard.php
@@ -275,8 +275,7 @@ class Dashboard extends Core\Controller {
     public function processMapCount(){
         $userID = $_POST['userid'];
         $mapcount = json_decode($this->getMapCount($userID));
-        // print_r($mapcount);
-        // exit;
+       
         $loadingCol=array();
         $loading = array(); 
         $sea=array();
