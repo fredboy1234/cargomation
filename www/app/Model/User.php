34551bc7668c0b41ae404a9b04580b4b2d0bb54a
neil-De <neildesucatan@gmail.com>
2022-05-23 16:42:34 +0800

    update dashboard sub account

--- www/app/Model/User.php
+++ www/app/Model/User.php
@@ -331,9 +331,17 @@ class User extends Core\Model {
 
     public function getUserSettings($user){
         $Db = Utility\Database::getInstance();
+        $check_user = $Db->query("SELECT *  FROM vrpt_subaccount where  user_id = '{$user}'")->results();
+        
+        $check_sub_user = $user;
+        if(isset($check_user[0]) && isset($check_user[0]->role_id)){
+            if($check_user[0]->role_id > 2 ){
+                $check_sub_user = $check_user[0]->account_id;
+            }
+        }
         return $Db->query("SELECT * 
                                 FROM user_settings
-                                WHERE user_id = '{$user}' ")->results();
+                                WHERE user_id = '{$check_sub_user}' ")->results();
     }
 
     public function deleteUserSettings($id){
