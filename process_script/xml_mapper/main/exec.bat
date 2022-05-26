5668bee2843f6f4d883f83fe9be7fd2bc2e80fb6
a2bfred <71646332+a2bfred@users.noreply.github.com>
2022-05-26 10:31:48 +0800

    Update: exec

--- process_script/xml_mapper/main/exec.bat
+++ process_script/xml_mapper/main/exec.bat
@@ -7,7 +7,7 @@ goto :eof
 if "%computername%"=="A2B-Cargomation" (
 
 for %%f in (C:/inetpub/wwwroot/process_script/xml_mapper/batch_file/*.bat) do (
-    SCHTASKS /CREATE /SC MINUTE /mo 1 /TN "\%%~nxf" /TR "C:\inetpub\wwwroot\process_script\xml_mapper\batch_file\%%~nxf" /f /ru A2B-Cargomation\Administrators /rp S@jn8cQ9dX7W!
+    SCHTASKS /CREATE /SC MINUTE /mo 1 /TN "\%%~nxf" /TR "C:\inetpub\wwwroot\process_script\xml_mapper\batch_file\%%~nxf" /f /ru A2B-Cargomation\A2B_Cargoadmin /rp S@jn8cQ9dX7W!
 )
 goto :eof
 )
