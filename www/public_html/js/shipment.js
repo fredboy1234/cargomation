ff001448f08faaea60dc893e6e685631af6284de
John Alex Ladra <johnalexladra@gmail.com>
2022-06-03 22:05:56 +0800

    fix shipment advance search

--- www/public_html/js/shipment.js
+++ www/public_html/js/shipment.js
@@ -729,6 +729,7 @@ $(document).ready(function () {
     $(".form_field_outer_row:not(:first)").remove();
     $("#no_value_1").val("");
     $("#no_search_1").val("");
+    $("#container_mode_1").val(null).trigger("change"); 
     $("#no_type_1").prop("selectedIndex", 0);
     $("#add_filters option").each(function(){
       $(this).attr("data-index",1);
@@ -1101,21 +1102,21 @@ function triggerType(data){
     break;
     case 'option':
         $('#'+data['id']).html(createSelect('others',data));
-        selectTrigger(data['value'],'select2');
+        selectTrigger(data['value'], 'select2', data['keyindex']);
     break;
 
     case 'document':
         $('#'+data['id']).html(createSelect('document',data));
-        selectTrigger(data['value'],'select2');
+        selectTrigger(data['value'], 'select2', data['keyindex']);
     break;
   }
 }
 
-function selectTrigger(index,inputType){
+function selectTrigger(index, inputType, search_type = ""){
   if(inputType === "select2"){
     $('#container_mode_'+index).select2();
-    var seach_type = $('#no_search_'+index+' option:selected').text();
-    if(seach_type == 'Type'){
+    // var seach_type = $('#no_search_'+index+' option:selected').text();
+    if(search_type == 'document:type'){
       let items = [];
       $.getJSON( "/document/getDocumentTypeByUserID/" + user_id, function( data ) {
         $.each( data, function( key, value ) {
