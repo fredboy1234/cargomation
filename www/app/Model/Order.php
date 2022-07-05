8ae39c820e399f1f3dee0815f51177094c579631
a2bfred <71646332+a2bfred@users.noreply.github.com>
2022-07-06 04:56:23 +0800

    Update: Converted tbl to Vrpt With custom QRY

--- www/app/Model/Order.php
+++ www/app/Model/Order.php
@@ -26,18 +26,18 @@ class Order extends Core\Model {
 
     public function getOrderData($user_id){
         $Db = Utility\Database::getInstance();
-        $query = "SELECT *  FROM orders WHERE user_id = '{$user_id}' AND status <>'INA'  ";
+        $query = "SELECT * FROM vrpt_Orders WHERE user_id = '{$user_id}' AND status <>'INA'  ";
         return $Db->query($query)->results();
     }
     public function getActiveOrdersCount($user_id){
         $Db = Utility\Database::getInstance();
-        $query = "SELECT count(*) as cnt FROM orders WHERE user_id = '{$user_id}' AND status <> 'DLV' ";
+        $query = "SELECT count(*) as cnt FROM vrpt_Orders WHERE user_id = '{$user_id}' AND status <> 'DLV' ";
         return $Db->query($query)->results();
     }
 
     public function getPendingOrder($user_id){
         $Db = Utility\Database::getInstance();
-        $query = "SELECT count(*) as cnt FROM orders 
+        $query = "SELECT count(*) as cnt FROM vrpt_Orders 
            WHERE user_id = '{$user_id}' 
            AND status not in ('SHP','DLV','INA') ";
         return $Db->query($query)->results();
@@ -45,7 +45,7 @@ class Order extends Core\Model {
 
     public function getOrderCountByStatus($user_id,$status){
         $Db = Utility\Database::getInstance();
-        $query = "SELECT count(*) as cnt FROM orders 
+        $query = "SELECT count(*) as cnt FROM vrpt_Orders 
            WHERE user_id = '{$user_id}' 
            AND status='{$status}'";
         return $Db->query($query)->results();
@@ -61,7 +61,7 @@ class Order extends Core\Model {
                     when status = 'DLV' Then 4
                     when status =  'CNF' Then 5
                 End ordering
-                FROM orders 
+                FROM vrpt_Orders 
            WHERE user_id = '{$user_id}' and status_desc <>'' 
            group by status_desc, status order by ordering asc";
  
@@ -70,7 +70,7 @@ class Order extends Core\Model {
 
     public function getFilterResults($user_id,$status){
         $Db = Utility\Database::getInstance();
-        $query = "SELECT * FROM orders 
+        $query = "SELECT * FROM vrpt_Orders 
            WHERE user_id = '{$user_id}' 
            AND status='{$status}' ";
         return $Db->query($query)->results();
@@ -78,7 +78,7 @@ class Order extends Core\Model {
 
     public function getOrderByShipment($user_id){
         $Db = Utility\Database::getInstance();
-        $query = "SELECT * FROM orders ord
+        $query = "SELECT * FROM vrpt_Orders ord
            INNER JOIN shipment sh on sh.order_number = ord.order_number
            WHERE sh.user_id = '{$user_id}'";
         return $Db->query($query)->results();
